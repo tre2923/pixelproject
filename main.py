5c966b602dfeb0e2c2cb13a038552145f4555202
tre2923 <tre2923@proton.me>
2024-11-12 23:28:15 -0500

    update main.py

--- main.py
+++ main.py
@@ -6,11 +6,3 @@ from game_logic import Game
 pygame.init()
 game = Game(screen_width=800, screen_height=600)
 game.run()
-# main.py
-
-import pygame
-from game_logic import Game
-
-pygame.init()
-game = Game(screen_width=800, screen_height=600)
-game.run()
